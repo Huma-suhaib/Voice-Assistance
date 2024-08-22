2b96311ebc430779fe4ddc6dc1c7d757a62437ab
Huma Suhaib <humasuhaib786@gmail.com>
2024-08-22 14:44:52 +0530

    Add files via upload
    Music Library

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "cat": "https://www.youtube.com/watch?v=nMwEyUZC7-0&list=RDnMwEyUZC7-0&start_radio=1",
+    "idol": "https://www.youtube.com/watch?v=H2JbPh1LtuM&list=RDnMwEyUZC7-0&index=7",
+    "my": "https://www.youtube.com/watch?v=LNmhC5K3Zy4&list=RDnMwEyUZC7-0&index=10"
+}
